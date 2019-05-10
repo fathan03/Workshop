be7c10c681097dcd509dd0f24c6957200deabba2
Fathan <s6030213033@phuket.psu.ac.th>
2019-05-11 00:49:59 +0700

    check login

---
+++ examples/login.js
@@ -0,0 +1,20 @@
+
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "admin" && password == "12345"){
+alert ("Login successfully");
+window.Slocation = "dashboard.html";  // Redirecting to other page.
+return false;
+}
+else{
+alert("Error login ");
+// Disabling fields after 3 attempts.
+// if( attempt == 0){
+// document.getElementById("username").disabled = true;
+// document.getElementById("password").disabled = true;
+// document.getElementById("submit").disabled = true;
+// return false;
+// }
+}
+}
